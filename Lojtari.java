b6abc667990b1898cef9437b56feb828997003ad
jetullahi <jetullahhaziraj6@gmail.com>
2021-09-11 12:37:02 +0200

    klasa lojtari

---
+++ Lojtari.java
@@ -0,0 +1,19 @@
+
+public class Lojtari {
+
+	private final String name;
+
+	public Lojtari(final String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+}
